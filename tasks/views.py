6ad60e3d38419acf83c2a5744defd2e8d51ef7c4
JonasDev <cardenasjdev@gmail.com>
2023-07-14 19:03:50 -0400

    Signin (iniciar sesion)

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
 from django.db import IntegrityError
 
@@ -15,7 +15,7 @@ from django.http import HttpResponse, HttpResponseServerError
 def home(request):
     return render(request, 'home.html')
 
-# Signup
+# Crear cuenta nueva
 
 
 def signup(request):
@@ -29,8 +29,8 @@ def signup(request):
                 user = User.objects.create_user(
                     username=request.POST['username'], password=request.POST['password1'])
                 user.save()
-                # Informacion del Usuario
-                login(request, user)    
+                # Informacion del Usuario. Guarda la sesion
+                login(request, user)
                 return redirect('tasks')
             except IntegrityError:
                 return render(request, 'signup.html', {
@@ -39,17 +39,47 @@ def signup(request):
                 })
         else:
             return render(request, 'signup.html', {
-                    'response': 'No coinciden las contraseñas',
-                    'form': UserCreationForm
-                })
+                'response': 'No coinciden las contraseñas',
+                'form': UserCreationForm
+            })
     # Metodo GET
-    else: 
+    else:
         return render(request, 'signup.html', {
             'form': UserCreationForm
         })
 
+# cerrar sesion
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+# permite iniciar sesion con cuentas ya creadas
+
+
+def signin(request):
+    # Metodo GET
+
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o contraseña es incorrecto'
+            })
+        else:
+            # Informacion del Usuario. Guarda la sesion
+            
+            login(request, user)
+            return redirect('tasks')
+
+
 def tasks(request):
     return render(request, 'tasks.html')
-
-def logout(request):
-    return render(request, 'logout.html')
